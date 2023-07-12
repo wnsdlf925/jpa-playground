3aa2d9d40d149fa3861d89eb6f2eb106d71b05fd
Junil <wnsdlf925@naver.com>
2023-07-12 17:48:12 +0900

    start

---
+++ jpa-playground/src/test/java/com/example/jpaplayground/JpaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
